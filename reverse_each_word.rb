7df720da726cb4388d985a5b724a626dcd726a9d
John Odom <joodom91@gmail.com>
2019-06-21 21:08:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(sentence1)
   new_sentence = split_array.join(" ")
   new_sentence
 end
+
+def reverse_each_word(sentence1)
+  split_array = sentence1.split(" ")
+  split_array.each do |word|
+    word.reverse!
+  end
+  new_sentence = split_array.join(" ")
+  new_sentence
+end
